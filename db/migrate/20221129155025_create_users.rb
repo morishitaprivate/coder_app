be10041a1731f22e52f5b0cfe57b2306ca905f9e
morishitaprivate <morisita140103@yahoo.co.jp>
2022-11-30 01:03:16 +0900

    issue/1 userモデル、テーブルの作成

---
+++ db/migrate/20221129155025_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :login_id, null: false
+      t.string :password_digest, null: false
+      t.integer :age
+      t.string :introduction
+
+      t.timestamps
+    end
+  end
+end
