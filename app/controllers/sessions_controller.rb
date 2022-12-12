540388a799fd3eb1bb014d88da1a18e6b5949c18
morishitaprivate <morisita140103@yahoo.co.jp>
2022-12-12 23:41:58 +0900

    #6 ログイン機能の実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(login_id: params[:session][:login_id].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash.now[:success] = 'ログインに成功しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ログインID、またはパスワードが異なります。'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
