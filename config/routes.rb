74e0275a8d08c177e902592c8f22cb264e5c5cb0
morishitaprivate <morisita140103@yahoo.co.jp>
2022-12-22 00:26:59 +0900

    #11 homeコントローラの作成とデフォルトルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  root to: 'sessions#new' # 一時的にルートはここにする。他にないため。
+  root to: 'home#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
