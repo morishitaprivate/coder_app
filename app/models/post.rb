4577251536a4a4a67049893d77722867bda5069f
morishitaprivate <morisita140103@yahoo.co.jp>
2022-12-23 01:25:26 +0900

    #16 enumerize設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,20 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :user_id, presence: true
+  validates :title, presence: true
+  validates :language, presence: true
   validates :content, presence: true
+  
+  enumerize :language, in: {
+    Cplus: 0,
+    Python: 1,
+    Java: 2,
+    C: 3,
+    Ruby: 4,
+    Csharp: 5,
+    Haskell: 6,
+    Rust: 7,
+    Other: 99,
+  }
+  
 end
