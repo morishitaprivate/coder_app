56435fa3d7398ad603ad814917a5fc422fc8e69d
morishitaprivate <morisita140103@yahoo.co.jp>
2023-01-01 22:58:19 +0900

    #27 軽い修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.order(id: 'DESC').limit(10)
-    @users = User.joins(:posts).group(:user_id).order('count(*) desc')
+    @users = User.joins(:posts).group(:user_id).order('count(*) desc').limit(10)
   end
 end
